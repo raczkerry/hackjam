b52eefc122078c7bcb4dd000f308e3c7c065161e
Racz Kerry <raczkerry@gmail.com>
2021-01-13 13:16:45 +0100

    Display all movies

--- src/App.tsx
+++ src/App.tsx
@@ -65,44 +65,16 @@ export function App({ categories, movies }: AppProps) {
           <div className='container mx-auto'>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10'>
               {/* Start: Movie Component */}
-              <div key={'movie-1'} className='single-movie relative'>
-                <img src='./image/poster.svg' alt={'movie title'} />
-                <div className='movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4'>
-                  <div className='content-inner'>
-                    <h3 className='mb-5'>{'Movie title'}</h3>
+              {movies.map(movie => (
+                <div key={movie.id} className='single-movie relative'>
+                  <img src={movie.poster_path} alt={movie.title} />
+                  <div className='movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4'>
+                    <div className='content-inner'>
+                      <h3 className='mb-5'>{movie.title}</h3>
+                    </div>
                   </div>
                 </div>
-              </div>
-              {/* End: Movie Component */}
-              {/* Start: Movie Component */}
-              <div key={'movie-2'} className='single-movie relative'>
-                <img src='./image/poster.svg' alt={'movie title'} />
-                <div className='movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4'>
-                  <div className='content-inner'>
-                    <h3 className='mb-5'>{'Movie title'}</h3>
-                  </div>
-                </div>
-              </div>
-              {/* End: Movie Component */}
-              {/* Start: Movie Component */}
-              <div key={'movie-3'} className='single-movie relative'>
-                <img src='./image/poster.svg' alt={'movie title'} />
-                <div className='movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4'>
-                  <div className='content-inner'>
-                    <h3 className='mb-5'>{'Movie title'}</h3>
-                  </div>
-                </div>
-              </div>
-              {/* End: Movie Component */}
-              {/* Start: Movie Component */}
-              <div key={'movie-4'} className='single-movie relative'>
-                <img src='./image/poster.svg' alt={'movie title'} />
-                <div className='movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4'>
-                  <div className='content-inner'>
-                    <h3 className='mb-5'>{'Movie title'}</h3>
-                  </div>
-                </div>
-              </div>
+              ))}
               {/* End: Movie Component */}
             </div>
           </div>
