d02ae4fa7e941280cc7dde279cbfca8db55f79d7
Racz Kerry <raczkerry@gmail.com>
2021-01-16 18:32:42 +0100

    Fix broken tests

--- test/App.spec.tsx
+++ test/App.spec.tsx
@@ -1,63 +1,74 @@
+import initialState from '../src/redux/initialState'
+import MovieCard from '../src/components/movieCard'
 import React from 'react'
 import { App } from '../src/App'
 import { IMovie } from '../src/types'
-import { shallow } from 'enzyme'
+import { Provider } from './testingUtils'
+import { movies as moviesMock } from '../src/mocks'
+import { mount, shallow } from 'enzyme'
 import 'jest-enzyme'
-
-const harryPotterAndThePhilosophersStoneMovie: IMovie = {
-  id: 1,
-  title: `Harry Potter and the Philosopher's Stone`,
-  genre_ids: [1, 2, 3],
-  backdrop_path: '',
-  poster_path: '/posters/harry-potter-and-the-philosopher-s-stone'
-}
-
-const harryPotterAndTheChamberOfSecretsMovie: IMovie = {
-  id: 2,
-  title: 'Harry Potter and Chamber of Secrets',
-  genre_ids: [1, 2, 3],
-  backdrop_path: '',
-  poster_path: '/posters/harry-potter-and-the-chamber-of-secrets'
-}
 
 test('App should render the movie title', () => {
   // Given
-  const movies: IMovie[] = [harryPotterAndThePhilosophersStoneMovie]
+  const movies: IMovie[] = [moviesMock[0]]
+  const state = {
+    ...initialState,
+    movies: { ...initialState.movies, filteredMovies: movies, movies }
+  }
 
   // When
-  const component = shallow(<App categories={[]} movies={movies} />)
+  const component = mount(
+    <Provider state={state}>
+      <App />
+    </Provider>
+  )
 
   // Then
-  expect(component).toContainReact(<h3 className='mb-5'>Harry Potter and the Philosopher's Stone</h3>)
+  expect(component).toContainReact(<MovieCard movie={movies[0]} />)
 })
 
 test('should render one movie title and poster', () => {
   // Given
-  const movies: IMovie[] = [harryPotterAndTheChamberOfSecretsMovie]
+  const movies: IMovie[] = [moviesMock[1]]
+  const state = {
+    ...initialState,
+    movies: { ...initialState.movies, filteredMovies: movies, movies }
+  }
 
   // When
-  const component = shallow(<App categories={[]} movies={movies} />)
+  const component = mount(
+    <Provider state={state}>
+      <App />
+    </Provider>
+  )
+  const movieCardComponent = shallow(<MovieCard movie={movies[0]} />)
 
   // Then
-  expect(component).toContainReact(<h3 className='mb-5'>Harry Potter and Chamber of Secrets</h3>)
-  expect(component).toContainReact(
-    <img src={'/posters/harry-potter-and-the-chamber-of-secrets'} alt={'Harry Potter and Chamber of Secrets'} />
-  )
+  expect(component).toContainReact(<MovieCard movie={movies[0]} />)
+  expect(movieCardComponent).toContainReact(<img src={movies[0].poster_path} alt={movies[0].title} />)
 })
 
 test('should render multiples movies title and poster', () => {
   // Given
-  const movies: IMovie[] = [harryPotterAndThePhilosophersStoneMovie, harryPotterAndTheChamberOfSecretsMovie]
+  const movies: IMovie[] = [moviesMock[0], moviesMock[1]]
+  const state = {
+    ...initialState,
+    movies: { ...initialState.movies, filteredMovies: movies, movies }
+  }
+
   // When
-  const component = shallow(<App categories={[]} movies={movies} />)
+  const component = mount(
+    <Provider state={state}>
+      <App />
+    </Provider>
+  )
 
   // Then
-  expect(component).toContainReact(<h3 className='mb-5'>Harry Potter and the Philosopher's Stone</h3>)
-  expect(component).toContainReact(<h3 className='mb-5'>Harry Potter and Chamber of Secrets</h3>)
-  expect(component).toContainReact(
-    <img src={'/posters/harry-potter-and-the-philosopher-s-stone'} alt={"Harry Potter and the Philosopher's Stone"} />
-  )
-  expect(component).toContainReact(
-    <img src={'/posters/harry-potter-and-the-chamber-of-secrets'} alt={'Harry Potter and Chamber of Secrets'} />
-  )
+  movies.forEach(movie => {
+    expect(component).toContainReact(<MovieCard movie={movie} />)
+
+    const movieCardComponent = shallow(<MovieCard movie={movie} />)
+
+    expect(movieCardComponent).toContainReact(<img src={movie.poster_path} alt={movie.title} />)
+  })
 })
